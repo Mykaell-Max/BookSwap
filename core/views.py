1d5d7ddacb906aa76877ea8e614326d52994866f
Mykaell Max <max000.256.000@gmail.com>
2025-02-21 14:01:04 -0300

    Done book and bookexchange logic and routing

--- core/views.py
+++ core/views.py
@@ -1,9 +1,27 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from django.contrib.auth.models import User
-from .models import UserProfile
-from .serializer import UserSerializer
+from .models import UserProfile, BookExchange, Book
+from .serializer import UserSerializer, BookSerializer, BookExchangeSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class BookExchangeViewSet(viewsets.ModelViewSet):
+    queryset = BookExchange.objects.all()
+    serializer_class = BookExchangeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(requester=self.request.user)
